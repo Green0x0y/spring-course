d618f82cbe35ec015294fcda0ec9e62d70d83ba2
Green0x0y <laurawiktor44@gmail.com>
2023-07-24 17:03:21 +0200

    values properties

--- myApp/src/main/java/spring/boot01/rest/FunRestController.java
+++ myApp/src/main/java/spring/boot01/rest/FunRestController.java
@@ -1,10 +1,17 @@
 package spring.boot01.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
     @GetMapping("/")
     public String sayHello(){
 
@@ -12,7 +19,6 @@ public class FunRestController {
     }
 
     // new endpoint for workout
-
     @GetMapping("/workout")
     public String getDailyWorkout(){
         return "Run a hard 5k!";
@@ -22,5 +28,10 @@ public class FunRestController {
     @GetMapping("/fortune")
     public String getDailyFortune(){
         return "Today is your lucky day!";
+    }
+
+    @GetMapping("/team")
+    public String getTeamAndCoach(){
+        return "Team " + teamName + " , Coach: " + coachName;
     }
 }
