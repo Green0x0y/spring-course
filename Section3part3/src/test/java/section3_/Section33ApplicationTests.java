166c4f3fc90c2e79c7667ecbec50689356e38ae1
Green0x0y <laurawiktor44@gmail.com>
2023-08-11 14:59:49 +0200

    new part

---
+++ Section3part3/src/test/java/section3_/Section33ApplicationTests.java
@@ -0,0 +1,13 @@
+package section3_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section33ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
