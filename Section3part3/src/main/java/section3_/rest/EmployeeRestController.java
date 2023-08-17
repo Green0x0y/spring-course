a7ecd3f45a098ebbd8cf876f7c2977429690fffb
Green0x0y <laurawiktor44@gmail.com>
2023-08-17 14:49:16 +0200

    service methods

--- Section3part3/src/main/java/section3_/rest/EmployeeRestController.java
+++ Section3part3/src/main/java/section3_/rest/EmployeeRestController.java
@@ -1,6 +1,7 @@
 package section3_.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import section3_.entity.Employee;
@@ -13,13 +14,22 @@ import java.util.List;
 public class EmployeeRestController {
     private EmployeeService employeeService;
 
-    public EmployeeRestController( EmployeeService theEmployeeService){
+    public EmployeeRestController(EmployeeService theEmployeeService) {
         employeeService = theEmployeeService;
 
     }
 
     @GetMapping("/employees")
-    public List<Employee> findAll(){
+    public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+        if (theEmployee == null) {
+            throw new RuntimeException(("Employee not found ") + employeeId);
+        }
+        return theEmployee;
     }
 }
