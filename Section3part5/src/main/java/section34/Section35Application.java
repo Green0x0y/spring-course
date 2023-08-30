cdc1f256e1aa49cec16f4760df7a790fecf3b804
Green0x0y <laurawiktor44@gmail.com>
2023-08-30 16:13:39 +0200

    magic-api

--- Section3part5/src/main/java/section34/Section35Application.java
+++ Section3part5/src/main/java/section34/Section35Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Section34Application {
+public class Section35Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Section34Application.class, args);
+		SpringApplication.run(Section35Application.class, args);
 	}
 
 }
