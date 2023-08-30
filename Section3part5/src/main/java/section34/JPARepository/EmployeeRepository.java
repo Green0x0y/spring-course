ce1f5f34788c781997ed96c0f7046e9f49ad07a5
Green0x0y <laurawiktor44@gmail.com>
2023-08-30 16:24:26 +0200

    sorting and pages

--- Section3part5/src/main/java/section34/JPARepository/EmployeeRepository.java
+++ Section3part5/src/main/java/section34/JPARepository/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package section34.JPARepository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import section34.entity.Employee;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
