78f13b5d8ae9cf6919ef47f158d6243e146851ff
Green0x0y <laurawiktor44@gmail.com>
2023-09-01 10:44:04 +0200

    security settings

--- Section5/00-spring-boot-rest-security-employee-starter-code/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java
+++ Section5/00-spring-boot-rest-security-employee-starter-code/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java
@@ -2,12 +2,14 @@ package com.luv2code.springboot.cruddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-public class CruddemoApplication {
+@EnableWebSecurity
+public class CruddemoApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
-
 }
